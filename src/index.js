45282f3cb54c101379237f24aec3ea603d1c7d81
sairama <sairamaj@hotmail.com>
2017-05-26 18:49:07 -0700

    pattern details in progress

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ var Alexa = require('alexa-sdk');
 var APP_ID = "amzn1.echo-sdk-ams.app.[amzn1.ask.skill.7b21ed76-1c8d-483b-ac6f-899fd16469df]";
 var SKILL_NAME = 'Tech Smart';
 
-var DesignPatternSummary = [
-    '<emphasis level="strong">Creational Patterns </emphasis>, Abstract Factory, Builder, Factory Method,Prototype, Singleton. ' +
-    '<break time="3s"/><emphasis level="strong"></emphasis> Structural Patterns, Adapter, Bridge , Composite, Decorator, Facade, Flyweight, Proxy. ' +
-    '<break time="3s"/><emphasis level="strong">Behavioral Patterns</emphasis>, Chain of Responsibility, Command, Interpreter, Iterator, Mediator, Memento, Observer, State, Strategy, Template Method, Visitor'
-];
+var DesignPatternSummary = 
+    '<emphasis level="moderate">Creational Patterns </emphasis>, Abstract Factory, Builder, Factory Method,Prototype, Singleton. '
+/*    
+var DesignPatternSummary = 
+    '<emphasis level="moderate">Creational Patterns </emphasis>, Abstract Factory, Builder, Factory Method,Prototype, Singleton. ' +
+    '<break time="1s"/><emphasis level="moderate"></emphasis> Structural Patterns, Adapter, Bridge , Composite, Decorator, Facade, Flyweight, Proxy. ' +
+    '<break time="1s"/><emphasis level="moderate">Behavioral Patterns</emphasis>, Chain of Responsibility, Command, Interpreter, Iterator, Mediator, Memento, Observer, State, Strategy, Template Method, Visitor.'
+*/
+var AskUserForSpecificPattern = "You can ask for specific pattern. For example tell me about Builder pattern."
+
+var patternsInfo = {}
+patternsInfo['singleton'] = 'This is singleton pattern'
+patternsInfo['builder'] = 'Builder pattern will be here'
+patternsInfo['adapter'] = 'Adapter will be explained here'
+patternsInfo['iterator'] = 'Iterator pattern is coming up.'
 
 exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -19,17 +29,28 @@ exports.handler = function (event, context, callback) {
 
 var handlers = {
     'LaunchRequest': function () {
-        this.emit('GetDesignPattern');
+        this.emit('DesignPatternSummary');
     },
-    'DesignPatternIntent': function () {
-        this.emit('GetDesignPattern');
+    'DesignPatternSummaryIntent': function () {
+        this.emit('DesignPatternSummary');
     },
-    'GetDesignPattern': function () {
+    'DesignPatternIntent' : function() {
+        this.emit('DesignPatternInfo')
+    },
+    'DesignPatternSummary': function () {
+        var speechOutput = DesignPatternSummary + AskUserForSpecificPattern
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, speechOutput)
+    },
+    'DesignPatternInfo': function () {
+        var patternName = this.event.request.intent.slots.name.value
         var speechOutput
-        var summary = DesignPatternSummary  [0];
-        // Create speech output
-        speechOutput = summary;
-        this.emit(':tellWithCard', speechOutput, SKILL_NAME, summary)
+        patternName = patternName.toLowerCase()
+        if( patternsInfo[patternName] === undefined ){
+            speechOutput = patternName + ' is not supported'
+        }else{
+            speechOutput = patternsInfo[patternName]
+        }
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, speechOutput)
     },
     'AMAZON.HelpIntent': function () {
         var speechOutput = "You can say give me last design pattern, or, you can say exit... What can I help you with?";
